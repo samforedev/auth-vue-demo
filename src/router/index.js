4957fdda2ed7488ff8e2f6b0a8dbed79069916bb
Samuel Forero <samuelf.dev@outlook.com>
2024-10-30 09:25:55 -0500

    Cri

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Login from '@/components/auth/Login.vue';
 import Register from '@/components/auth/Register.vue';
 import DashBoard from '@/components/dashboard/DashBoard.vue';
-import Users from "@/components/dashboard/Users.vue";
+import User from '@/components/Users/User.vue';
 
 const routes = [
     { path: '/', component: Login },
     { path: '/login', name: 'Login', component: Login },
     { path: '/register', name: 'Register', component: Register },
     { path: '/dashboard', name: 'Dashboard', component: DashBoard },
-    { path: '/users', name: 'Users', component: Users},
-
+    { path: '/users', name: 'Users', component: User }
 ];
 
 const router = createRouter({
